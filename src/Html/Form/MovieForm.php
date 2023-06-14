30f51c38a7611e60633a3fc6b6dfde2385523992
Thibault MINNEBOO <thibault.minneboo@etudiant.univ-reims.fr>
2023-06-14 18:49:45 +0200

    add: Ajout de la méthode getHtmlForm() pour le formulaire de film

--- src/Html/Form/MovieForm.php
+++ src/Html/Form/MovieForm.php
@@ -4,6 +4,7 @@ declare(strict_types=1);
 namespace Html\Form;
 
 use Entity\Movie;
+use Exception;
 use Html\StringEscaper;
 
 class MovieForm
@@ -35,5 +36,33 @@ class MovieForm
         return $this->movie;
     }
 
+    /**
+     * @throws Exception
+     */
+    public function getHtmlForm(string $action): string
+    {
+        $id = $this->movie ? $this->getMovie()->getId() : '';
+        $title = $this->movie ? $this->getMovie()->getTitle() : '';
+        $runtime = $this->movie ? $this->getMovie()->getRuntime() : '';
+        $tagline = $this->movie ? $this->getMovie()->getTagline() : '';
+        $releaseDate = $this->movie ? date_format($this->getMovie()->getReleaseDate(), 'Y-m-d') : '';
+        $overview = $this->movie ? $this->getMovie()->getOverview() : '';
 
+        return <<<HTML
+        <form action="{$action}" method="post" class="form__movie">
+            <input type="hidden" name="id" value="{$id}" class="form__input">
+            <label for="title" class="form__label">Titre</label>
+            <input type="text" name="title" value="{$title}" class="form__input">
+            <label for="tagline" class="form__label">Slogan</label>
+            <input type="text" name="tagline" value="{$tagline}" class="form__input">
+            <label for="runtime" class="form__label">Durée</label>
+            <input type="number" name="runtime" value="{$runtime}" class="form__input">
+            <label for="releaseDate" class="form__label">Date de mise en salle</label>
+            <input type="date" name="releaseDate" value="{$releaseDate}" class="form__input">
+            <label for="overview" class="form__label">Résumé</label>
+            <textarea name="overview" cols="30" rows="10" class="form__input">{$overview}</textarea>
+            <button type="submit">Enregistrer</button>
+        </form>
+        HTML;
+    }
 }
